ec1a5c4e32e6d69dbaad51012b5ccff2fa9b327d
arghi082k1 <arghyasaha08@gmail.com>
2021-12-07 20:11:10 +0530

    Hello

---
+++ src/containers/App.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+import { robots } from '../robots';
+import Scroll from '../components/Scroll';
+import ErrorBoundary from '../components/ErrorBoundary';
+import './App.css';
+
+
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      robots: robots,
+      searchfield: ''
+    }
+  }
+
+  onSearchChange = (event) => {
+    this.setState({ searchfield: event.target.value })
+  }
+
+  
+
+  render(){
+    const { robots, searchfield } = this.state;
+      const filteredRobots = robots.filter(robot =>{
+          return robot.name.toLowerCase().includes(searchfield.toLowerCase())
+      })
+        
+  
+    return (
+      
+        <div className='tc'>
+          <h1 className='f2'>RoboFriends</h1>
+          <SearchBox searchChange={this.onSearchChange}/>
+          <Scroll>
+            <ErrorBoundary>
+           <CardList robots={filteredRobots} />
+            </ErrorBoundary>
+          </Scroll>
+        </div>
+      );
+  }
+}
+
+export default App;
